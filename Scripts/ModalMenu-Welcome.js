ab3ed575e333f19f8df0490751551367c92749ee
Starfall <123963378+sylfurgames@users.noreply.github.com>
2024-01-02 21:22:31 +0300

    Delete Scripts directory

--- Scripts/ModalMenu-Welcome.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var modal = document.querySelector('.modal-container');
-    var closeModalBtn = document.querySelector('#close-modal');
-  
-    closeModalBtn.addEventListener('click', function() {
-      modal.style.display = 'none';
-    });
-  });
